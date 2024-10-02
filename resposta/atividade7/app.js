627e49b0dde86a81450e170244de60bad47d19ea
daviiisousa <sousadavi248@gmail.com>
2024-10-02 11:05:58 -0300

    atividade 7 resolvida

---
+++ resposta/atividade7/app.js
@@ -0,0 +1,12 @@
+// Faça  um  algoritmo  que  leia  uma  variável  e  some  5  caso  seja  par  ou  some  8  caso  seja  ímpar,  
+// imprimir o resultado desta operação. 
+
+let numero = 5
+
+if(numero % 2 == 0){
+    let resultado = numero + 5
+    console.log(resultado)
+}else{
+   let resultado = numero + 8
+   console.log(resultado)
+}

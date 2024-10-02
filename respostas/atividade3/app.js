b2ac56bcd9ff8304df0f9459aa0d4d9666951093
JohnMsousa <johnmsousasantos@gmail.com>
2024-10-02 10:49:00 -0300

    Atividade 4

--- respostas/atividade3/app.js
+++ respostas/atividade3/app.js
@@ -1,6 +1,6 @@
 var numero = prompt("Digite um número");
 
-if (numero % 2 == 0) {
+if (numero % 2 === 0) {
     console.log("Par");
 } else {
     console.log("ímpar");
---
+++ respostas/atividade4/app.js
@@ -0,0 +1,9 @@
+var A = Number(prompt("Digite um número inteiro"));
+var B = Number(prompt("Digite um número inteiro"));
+if (A == B) {
+    let C = A + B;
+    console.log(C);
+} else {
+    let C = A * B
+    console.log(C);
+}

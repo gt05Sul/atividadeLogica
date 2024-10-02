bc5bc5e9b3f25a11f1a93534088bf2f1c4e90507
daviiisousa <sousadavi248@gmail.com>
2024-10-02 09:38:22 -0300

    atividade 1 resolvida

---
+++ resposta/atividade1/app.js
@@ -0,0 +1,11 @@
+let A = 5
+let B = 10
+let C = 30
+
+let somaDeAeB = A + B
+
+if ( somaDeAeB < C){
+    console.log(`O valo de C e maior que e a soma de A e B `)
+}else{
+    console.log("a soma de A e B e maior que C")
+}

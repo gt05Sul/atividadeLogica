02ef6ebc06ae78f3dcb223d530bf4bbfda8aeece
Ingrid <stephanye.ingrid@gmail.com>
2024-10-02 10:43:55 -0300

    Resolução do exercício 4 - Lista de condicionais

---
+++ respostas/condicionais/ex4.js
@@ -0,0 +1,15 @@
+
+// Faça  um  algoritmo  que  leia  dois  valores  inteiros  A  e  B,  se  os  valores  forem  iguais  deverá  se  
+// somar  os  dois,  caso  contrário  multiplique  A  por  B.  Ao  final  de  qualquer  um  dos  cálculos  deve-se  
+// atribuir o resultado para uma variável C e mostrar seu conteúdo na tela.
+
+let valor1 = 5;
+let valor2 = 4;
+
+if(valor1 === valor2){
+    let valor3 = valor1 + valor2;
+    console.log(valor3);
+}else{
+    let valor3 = valor1 * valor2;
+    console.log(valor3);
+}

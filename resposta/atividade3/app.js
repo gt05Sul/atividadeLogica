584a379ff75ba13994edc2d144c785da98629832
daviiisousa <sousadavi248@gmail.com>
2024-10-02 09:46:28 -0300

    atividade 3 resolvida

---
+++ resposta/atividade3/app.js
@@ -0,0 +1,7 @@
+// Faça um algoritmo para receber um número qualquer e informar na tela se é par ou ímpar. 
+let numero = 4
+if(numero % 2 == 0){
+    console.log("seu numero e par")
+}else{
+    console.log("seu numero e ímpar");
+}

1e930cdd4c905cd8a3ab723e75323c1e4f25a069
Jao Ryan <joaoryanoliveira200@gmail.com>
2024-10-02 11:07:41 -0300

    questao4

---
+++ Logica-ryan/questao4/app.js
@@ -0,0 +1,16 @@
+// 4)  Faça  um  algoritmo  que  leia  dois  valores  inteiros  A  e  B  se  os  valores  forem  iguais  deverá  se  
+// somar  os  dois,  caso  contrário  multiplique  A  por  B.  Ao  final  de  qualquer  um  dos  cálculos  deve-se  
+// atribuir o resultado para uma variável C e mostrar seu conteúdo na tela
+
+
+let A = 88
+let B = 55
+
+let C = ""
+if ( A === B){
+    C = A + B 
+   console.log(C)
+} else if (A != B){
+    C = A*B
+    console.log(C)
+}

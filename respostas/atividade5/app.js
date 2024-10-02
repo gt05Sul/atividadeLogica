2820e6e50bbeb9fcfaae1a3e9f540f2f30220a40
JohnMsousa <johnmsousasantos@gmail.com>
2024-10-02 11:11:50 -0300

    Atividade 5

---
+++ respostas/atividade5/app.js
@@ -0,0 +1,9 @@
+var valor = Number(prompt("Digite um número inteiro"));
+
+if (valor >= 0) {
+    valor = valor * 2;
+    console.log(valor);
+} else { (valor > 0);
+    valor = valor * 3;
+    console.log(valor);
+}

de6a01fd740760b768c3faa607093ce2a05db06d
daviiisousa <sousadavi248@gmail.com>
2024-10-02 10:42:03 -0300

    atividade 5 resolvida

---
+++ resposta/atividade5/app.js
@@ -0,0 +1,14 @@
+// Encontrar  o  dobro  de  um  número  caso  ele  seja  positivo  e  o  seu  triplo  caso  seja  negativo,  
+// imprimindo o resultado.
+
+let numero = 5
+
+if( numero > 0){
+    let dobroDoNumero = numero * 2
+    console.log(dobroDoNumero);
+    
+}else{
+    let triploDoNumero = numero * 3
+    console.log(triploDoNumero);
+    
+}

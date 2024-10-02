ca3e320492d8f2104db7a6f0fa63fddeff918250
Ingrid <stephanye.ingrid@gmail.com>
2024-10-02 09:30:27 -0300

    Resolução do exercício 1 - Lista de condicionais

---
+++ respostas/condicionais/ex1.js
@@ -0,0 +1,15 @@
+
+// Faça um algoritmo que leia os valores A, B, C e imprima na tela se a soma de A + B é menor 
+// que C.
+
+let valor1 = 4;
+let valor2 = 5;
+let valor3 = 10;
+
+let soma = valor1 + valor2;
+
+if(soma > valor3){
+    console.log(`A soma dos valores é maior que ${valor3}`);
+}else{
+    console.log(`A soma dos valores é menor que ${valor3}`);
+}

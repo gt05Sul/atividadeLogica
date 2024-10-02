045ac8a62a844d70ed01b6306202c290f6003f0c
HeyVds <vitorsan642@gmail.com>
2024-10-02 08:54:21 -0300

    exercicio 01

---
+++ Atividades VictorM/exercicio.js
@@ -0,0 +1,11 @@
+// exercicio 1
+
+// const a = Number(prompt("Digite o primeiro valor: "))
+// const b = Number(prompt("Digite o segundo valor: "))
+// const c = Number(prompt("Digite o terceiro valor valor: "))
+
+// if (a + b < c) {
+//     console.log("A + B é menor que C");
+// } else {
+//     console.log("A + B é maior que C");
+// }

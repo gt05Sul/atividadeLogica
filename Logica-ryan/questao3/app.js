f0660be9beff2ad1869318715719577fb7a42ea1
Jao Ryan <joaoryanoliveira200@gmail.com>
2024-10-02 10:49:25 -0300

    questao 3

---
+++ Logica-ryan/questao3/app.js
@@ -0,0 +1,9 @@
+// 3) Faça um algoritmo para receber um número qualquer e informar na tela se é par ou ímpar.
+
+let numero = 5
+
+if (numero % 2 == 0){
+    console.log(`O numero ${numero} é par`)
+}   else {
+    console.log(`o numero ${numero} é impar`)
+}

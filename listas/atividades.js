a57c23bb466c1b8f7cea979ec08b0a56b5343ca6
vingenesi <vinnyvvv04@gmail.com>
2024-10-02 09:53:09 -0300

    questão 2 resolvida

--- listas/atividades.js
+++ listas/atividades.js
@@ -23,13 +23,25 @@
 
 //Questão 1
 
-var valora = 3;
-var valorb = 3;
-var valorc = 7;
+// var valora = 3;
+// var valorb = 3;
+// var valorc = 7;
 
-const soma = valora + valorb;
-if (soma < valorc) {
-    console.log("A soma de A+B é menor que C");
-} else {
-    console.log("A soma de A+B é maior que C");
+// const soma = valora + valorb;
+// if (soma < valorc) {
+//     console.log("A soma de A+B é menor que C");
+// } else {
+//     console.log("A soma de A+B é maior que C");
+// }
+
+//Questão 2
+
+var nome = "Maria";
+var sexo = "F";
+var estadocivil = "Casada";
+
+if (sexo == "F" && estadocivil == "Casada") {
+    console.log("A quantos anos você é casada?");
+}else {
+    console.log("tudo bem");
 }

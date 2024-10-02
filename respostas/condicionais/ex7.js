63297365600d5bf58dd661c5d0aefe290ba41c41
Ingrid <stephanye.ingrid@gmail.com>
2024-10-02 11:03:26 -0300

    Resolução do exercicio 7 - Lista de condicionais

---
+++ respostas/condicionais/ex7.js
@@ -0,0 +1,13 @@
+
+// Faça  um  algoritmo  que  leia  uma  variável  e  some  5  caso  seja  par  ou  some  8  caso  seja  ímpar,  
+// imprimir o resultado desta operação.
+
+let num = 3;
+
+if(num % 2 == 0){
+    let par = num + 5;
+    console.log (par);
+} else{
+    let impar = num + 8;
+    console.log (impar);
+}

462d9a6075a7748b394eb34a6e874506c18838d2
daviiisousa <sousadavi248@gmail.com>
2024-10-02 10:58:16 -0300

    atividade 6 resolvida

---
+++ resposta/atividade6/app.js
@@ -0,0 +1,10 @@
+// Escreva um algoritmo que lê dois valores booleanos (lógicos) e então determina se ambos são 
+// VERDADEIROS ou FALSOS.
+
+let verdadeiroOuFalso;
+
+if(verdadeiroOuFalso === true){
+    console.log("Verdadeiro")
+}else{
+    console.log("falso")
+}

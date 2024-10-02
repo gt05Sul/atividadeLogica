f4a369e4b2fb105050a0471c39994c3bae6b4db9
Jao Ryan <joaoryanoliveira200@gmail.com>
2024-10-02 11:28:23 -0300

    questao5

---
+++ Logica-ryan/questao5/app.js
@@ -0,0 +1,12 @@
+// 5)  Encontrar  o  dobro  de  um  número  caso  ele  seja  positivo  e  o  seu  triplo  caso  seja  negativo,  
+// imprimindo o resultado. 
+
+let A = -4
+
+if (A > 0){
+    let dobro = A*2
+    console.log(`O resultado positivo é ${dobro}`)
+} else {
+    let triplo = A*3
+    console.log(` O resultado Negativo é ${triplo}`)
+}

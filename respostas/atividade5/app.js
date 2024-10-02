1cac46189eb634e1370eaedb99ff82c7cbd04139
JohnMsousa <johnmsousasantos@gmail.com>
2024-10-02 11:58:38 -0300

    Atividade 6

--- respostas/atividade5/app.js
+++ respostas/atividade5/app.js
@@ -3,7 +3,7 @@ var valor = Number(prompt("Digite um número inteiro"));
 if (valor >= 0) {
     valor = valor * 2;
     console.log(valor);
-} else { (valor > 0);
+} else {
     valor = valor * 3;
     console.log(valor);
 }
---
+++ respostas/atividade6/app.js
@@ -0,0 +1,15 @@
+var valor1 = prompt("V ou F?");
+var valor2 = prompt("V ou F?");
+
+// let valorA = "Verdadeiro"
+// let valorB = "Falso"
+
+
+
+if (valor1 == "V" && valor2 ==  "V") {
+    console.log("Verdadeiros");
+    
+} else {
+    console.log("Falsos");
+}
+

f21eb82ac2b9178e64368ea8d9382d37f83054f9
Jao Ryan <joaoryanoliveira200@gmail.com>
2024-10-02 13:34:18 -0300

    questao8-cornoJob

---
+++ Logica-ryan/questao 8/app.js
@@ -0,0 +1,28 @@
+// 8)  Escreva  um  algoritmo  que  leia  três  valores  inteiros  e  diferentes  e  mostre-os  em  ordem  
+// decrescente. 
+
+
+
+let A = 4
+let B = 7
+let C = 10
+
+if (A > B && A > C){
+    if (B > C){
+        console.log (`ordem decrescente: ${A} , ${B} , ${C}`)
+    } else {
+        console.log(`Ordem decrescente: ${A} , ${C} , ${B} `)
+    }
+} else if (B > A && B > C){
+    if(A > C){
+        console.log(`Orde decrescente: ${B} , ${A} , ${C}`)
+    } else {
+        console.log(`Ordem decrescente: ${B} , ${C} , ${A}`)
+    }
+} else {
+    if (A > B){
+        console.log(`Ordem decrescente: ${C}, ${A} , ${B}`)
+    } else {
+        console.log(`Ordem decrescente: ${C}, ${B} , ${A}`)
+    }
+}

48eab60902f04c0cd79b42911656e33dabcdf072
Jao Ryan <joaoryanoliveira200@gmail.com>
2024-10-02 11:32:23 -0300

    questao6

---
+++ Logica-ryan/questao6/app.js
@@ -0,0 +1,11 @@
+// 6) Escreva um algoritmo que lê dois valores booleanos (lógicos) e então determina se ambos são 
+// VERDADEIROS ou FALSOS.
+
+const v = true
+const f = false 
+
+if (v == true && f == false){
+    console.log("true")
+} else {
+    console.log("false")
+}

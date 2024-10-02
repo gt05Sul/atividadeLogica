2517b3ea895f5511f83551c6038607a41ec929be
Ingrid <stephanye.ingrid@gmail.com>
2024-10-02 10:48:41 -0300

    Resoluções do exercício 5 - Loista de condicionais

---
+++ respostas/condicionais/ex5.js
@@ -0,0 +1,13 @@
+
+// Encontrar  o  dobro  de  um  número  caso  ele  seja  positivo  e  o  seu  triplo  caso  seja  negativo,  
+// imprimindo o resultado./
+
+let num = -5;
+
+if(num >0){
+    let dobro = 2*num;
+    console.log (dobro);
+}  else {
+    let triplo = 3*num;
+    console.log (triplo);
+}

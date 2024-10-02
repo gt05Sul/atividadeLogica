8e103cbb61e028e284f97b731792a6210cf40560
jairochabr <JAIRO0803@digitalcollegesul.local>
2024-10-02 09:53:29 -0300

    4)  Faça  um  algoritmo  que  leia  dois  valores  inteiros  A  e  B  se  os  valores  forem  iguais  deverá  se somar  os  dois,  caso  contrário  multiplique  A  por  B.  Ao  final  de  qualquer  um  dos  cálculos  deve-se atribuir o resultado para uma variável C e mostrar seu conteúdo na tela.

--- lista-de-condicionais/exercicios.js
+++ lista-de-condicionais/exercicios.js
@@ -1,7 +1,7 @@
 /* 1) Faça um algoritmo que leia os valores A, B, C e imprima na tela se a soma de A + B é menor 
 que C. */
 
-const A = 3
+/* const A = 3
 const B = 2
 const C = 6
 
@@ -9,7 +9,7 @@ const soma = A + B
 
 if (soma < C) {
     console.log(`O resultado da soma de A + B = ${soma}`)
-}
+} */
 
 
 /* 2) Faça um algoritmo que leia o nome, o sexo e o estado civil de uma pessoa.
@@ -33,3 +33,24 @@ const valor = 5
 const verificaValor = valor % 2 === 0 ? 'PAR' : "IMPAR"
 
 console.log(verificaValor)
+
+
+/* 4)  Faça  um  algoritmo  que  leia  dois  valores  inteiros  A  e  B  se  os  valores  forem  iguais  deverá  se  
+somar  os  dois,  caso  contrário  multiplique  A  por  B.  Ao  final  de  qualquer  um  dos  cálculos  deve-se  
+atribuir o resultado para uma variável C e mostrar seu conteúdo na tela.  */
+
+const A = 3
+const B = 4
+
+function soma() {
+    if (A === B) {
+        let soma = A + B
+        return soma
+    } else {
+        let soma = A * B
+        return soma
+    }
+}
+
+let C = soma()
+console.log(C)

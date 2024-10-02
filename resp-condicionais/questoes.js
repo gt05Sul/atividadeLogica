b1e42df7faaa65d38f1f96a471dce92ca03e1fde
unknown <LYSANDRE0894@digitalcollegesul.local>
2024-10-02 09:16:04 -0300

    if eu entregar a atividade, o professor fica feliz, else, ele fica triste.

---
+++ resp-condicionais/questoes.js
@@ -0,0 +1,20 @@
+
+// ---------------------------- Questao 1 ------------------------------------
+
+// lendo os valores
+
+// const a = 20;
+// const b = 12;
+// const c = 10;
+
+// condicional
+
+// if (a + b > c) {
+//     console.log ("A somado a B é maior do que C.");
+// } else {
+//     console.log ("A somado a B é menor do que C.");
+// }
+
+
+
+

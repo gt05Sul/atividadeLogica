b0ee5ef14c7c2e302d0dd61ffc414220545d15e6
Ingrid <stephanye.ingrid@gmail.com>
2024-10-02 10:58:13 -0300

    Resolução do exercicio 6 - Lista de condicionais

---
+++ respostas/condicionais/ex6.js
@@ -0,0 +1,12 @@
+
+// Escreva um algoritmo que lê dois valores booleanos (lógicos) e então determina se ambos são 
+// VERDADEIROS ou FALSOS.
+
+const Verdadeiro = true;
+const Falso = true;
+
+if (Verdadeiro === true && Falso ===  false){
+    console.log ("Verdadeiro");
+} else {
+    console.log ("Falso");
+}

ed7f429043e2a9f7826010b67ba69f861ad09de3
Ingrid <stephanye.ingrid@gmail.com>
2024-10-02 18:20:01 -0300

    exercício 8 - atualizado

--- respostas/condicionais/ex8.js
+++ respostas/condicionais/ex8.js
@@ -1,28 +1,14 @@
 
 // Escreva  um  algoritmo  que  leia  três  valores  inteiros  e  diferentes  e  mostre-os  em  ordem decrescente.
 
-let valor1 = -5;
-let valor2 = 5;
-let valor3 = 20;
+let valor1 = 8;
+let valor2 = 9;
+let valor3 = 10;
 
-if(valor1 > valor2 && valor2 > valor3){
-    console.log(valor1, valor2, valor3);
-} 
-else if(valor1 > valor2 && valor3 > valor2){
-    console.log(valor1, valor3, valor2);
-} 
-else if(valor2 > valor1 && valor1 > valor3){
-    console.log(valor2, valor1, valor3);
-} 
-else if(valor2 > valor1 && valor3 > valor1){
-    console.log(valor2, valor3, valor1);
-} 
-else if(valor3 > valor2 && valor2 > valor1){
-    console.log(valor3, valor2, valor1);
-} 
-else if(valor3 > valor2 && valor1 > valor2){
-    console.log(valor3, valor1, valor2);
-} 
-else {
-    console.log(erro);
-}
+let valores = [valor1, valor2, valor3];
+
+valores.sort(function(x, y) {
+    return y - x; 
+});
+
+console.log(valores);

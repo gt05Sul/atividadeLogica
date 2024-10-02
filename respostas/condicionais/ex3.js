013723acb0ffe05e550cb90fbd2fb5f6f6698b9d
Ingrid <stephanye.ingrid@gmail.com>
2024-10-02 09:54:55 -0300

    Resolução do exercício 3 - Lista de condicionais

---
+++ respostas/condicionais/ex3.js
@@ -0,0 +1,10 @@
+
+// Faça um algoritmo para receber um número qualquer e informar na tela se é par ou ímpar.
+
+let num = 3;
+
+if(num % 2 == 0){
+    console.log(`O número ${num} é par`);
+} else {
+    console.log(`O número ${num} é ímpar`);
+}

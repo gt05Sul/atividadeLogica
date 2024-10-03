1002a79e9cd8f13ebad27fa8a06a81587548f97e
Jao Ryan <joaoryanoliveira200@gmail.com>
2024-10-03 09:50:31 -0300

    pastalogica

---
+++ Logica-ryan/repetiçao/app.js
@@ -0,0 +1,36 @@
+// let contador = 0
+// while (contador <= 10){
+//     console.log(contador)
+//   contador++ 
+// }
+
+
+    // do {
+    //     console.log(contador)
+    //     contador++
+    // } while (contador <= 10)
+
+
+
+    // const num = [20 , 30 , 50 , 100];
+    // let soma = 0;
+
+    // for(let i = 0 ; i < num.length ; i++){
+    //    soma += num[i]
+    // }   
+    // console.log(soma)
+
+
+
+
+let frutas = ['ovo' ,  'pimenta' , 'abacate' , 'peixe']
+
+// for(let fruta of frutas) {
+//     console.log(fruta)
+// }
+
+
+for (let fruta in frutas) {
+    console.log(fruta)
+}
+

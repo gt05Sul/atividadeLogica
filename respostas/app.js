c28ca632cb8e06417274682c4261d16f18cd9ef2
carloeira <augustovieira.cb@gmail.com>
2024-10-03 10:16:45 -0300

    resolucao 11 e 12

--- respostas/app.js
+++ respostas/app.js
@@ -26,3 +26,61 @@ function tempoCasada() {
 }
 // tempoCasada()
 
+function verDesconto() {
+    let produto = parseFloat(readline.question("Digite o valor do produto de acordo com a etiqueta: "));
+    let formaPagamento = parseInt(readline.question("Informe a forma de pagamento: (1) A vista 10% - (2) Cartao 15% - (3) Normal 0% - (4) Juros de 10% "));
+
+    let precoFinal;
+
+    switch (formaPagamento) {
+        case 1: precoFinal = produto * 0.9; break;
+        case 2: precoFinal = produto * 0.85; break;
+        case 3: precoFinal = produto; break;
+        case 4: precoFinal = produto * 1.1; break;
+    }
+
+    console.log(`
+            De acordo com a forma de pagamento,
+            o total da sua compra ficou em ${precoFinal.toFixed(2)}
+        `)
+}
+// verDesconto()
+
+function verificaMedia() {
+    let matricula = readline.question("Informe sua matricula: ");
+    let nota1 = parseFloat(readline.question("Qual nota da av1?"));
+    let nota2 = parseFloat(readline.question("Qual nota da av2?"));
+    let nota3 = parseFloat(readline.question("Qual nota da av3?"));
+    let me = parseFloat(readline.question("Qual a média de exercícios?"));
+
+    let ma = (nota1 + (nota2 * 2) + (nota3 * 3) + me)/7;
+
+    let conceito;
+    switch (true) {
+        case(ma >= 9):
+            conceito = "A";
+            break;
+        case(ma >= 7.5):
+            conceito = "B";
+            break;
+        case(ma >= 6):
+            conceito = "C";
+            break;
+        case(ma >= 4):
+            conceito = "D";
+            break;
+        default:
+            conceito = "E";
+            break;
+    }
+
+    let status = (conceito === "A" || conceito === "B" || conceito === "C") ? "Aprovado" : "Reprovado";
+
+    console.log(`
+            Referente à matricula: ${matricula},
+            a média final foi ${ma.toFixed(2)},
+            Conceito: ${conceito},
+            status: ${status}
+        `);
+}
+verificaMedia()

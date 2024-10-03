d2b6586f67a4998fc4a60474c6cf0011890b1c0f
Jao Ryan <joaoryanoliveira200@gmail.com>
2024-10-03 09:50:02 -0300

    questao1

--- Logica-ryan/questão1/app.js
+++ Logica-ryan/questão1/app.js
@@ -2,15 +2,18 @@
 
 // 1) Faça um algoritmo que leia os valores A, B, C e imprima na tela se a soma de A + B é menor 
 // que C.
-    let A = 10
-    let B = 28
-    let C = 100
+    // let A = 10
+    // let B = 28
+    // let C = 100
 
-    let valor = A + B
+    // let valor = A + B
 
-        if (valor < C){
-            console.log(`${valor} é menor que C`)
-        } else {
-            console.log(`${valor} é maior que C`)
+    //     if (valor < C){
+    //         console.log(`${valor} é menor que C`)
+    //     } else {
+    //         console.log(`${valor} é maior que C`)
            
-        }
+    //     }
+
+
+       

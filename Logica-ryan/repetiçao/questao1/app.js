494c972227a74594253a14b33ac3c89a26641b24
Jao Ryan <joaoryanoliveira200@gmail.com>
2024-10-03 13:44:03 -0300

    questao 1

---
+++ Logica-ryan/repetiçao/questao1/app.js
@@ -0,0 +1,16 @@
+// 1) Desenvolver um algoritmo que efetue a soma de todos os números ímpares que são múltiplos de 
+// três e que se encontram no conjunto dos números de 1 até 500
+
+// Inicializa um array para armazenar os números
+    let num = []
+
+    // Percorre todos os números de 1 a 500
+    for (let prox = 1; prox <= 500 ; prox++){
+        if (prox % 2 !== 0 && prox % 3 === 0){
+            num.push(prox)
+        }
+    }
+
+    let somar = num.reduce((acc , curr) => acc + curr , 0);
+
+    console.log(`A soma dos números é ímpares que são múltiplos de 3 de 1 a 500 é: ${somar} `)

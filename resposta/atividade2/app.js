dc0976b9a346bd3b14c81f78d37cf837bbddce34
daviiisousa <sousadavi248@gmail.com>
2024-10-03 09:58:28 -0300

    atividade 2 feita

--- resposta/atividade2/app.js
+++ resposta/atividade2/app.js
@@ -1,7 +1,7 @@
 // Faça um algoritmo que leia o nome, o sexo e o estado civil de uma pessoa. Caso sexo seja “F” e 
 // estado civil seja “CASADA”, solicitar o tempo de casada (anos).
 
-const nome = "Davi"
+const nome = "eduarda"
 const sexo = "F"
 const estadoCivil = "CASADA"
 

739b389bb2b1b25c6d31ebc4c28a2603bb6b7e3f
Jao Ryan <joaoryanoliveira200@gmail.com>
2024-10-04 08:37:53 -0300

    questao1

--- Logica-ryan/repetiçao/questao1/app.js
+++ Logica-ryan/repetiçao/questao1/app.js
@@ -2,15 +2,21 @@
 // três e que se encontram no conjunto dos números de 1 até 500
 
 // Inicializa um array para armazenar os números
-    let num = []
+    // let num = []
+
+    let soma = 0;
 
     // Percorre todos os números de 1 a 500
     for (let prox = 1; prox <= 500 ; prox++){
         if (prox % 2 !== 0 && prox % 3 === 0){
-            num.push(prox)
+            // num.push(prox)
+            soma += prox;
+            console.log(prox)
         }
+
     }
 
-    let somar = num.reduce((acc , curr) => acc + curr , 0);
 
-    console.log(`A soma dos números é ímpares que são múltiplos de 3 de 1 a 500 é: ${somar} `)
+    // let somar = num.reduce((acc , curr) => acc + curr , 0);
+
+    console.log(`A soma dos números é ímpares que são múltiplos de 3 de 1 a 500 é: ${soma} `)

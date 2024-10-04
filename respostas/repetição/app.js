06232bc25462be9217395d1f7f7827f7eeb758d8
Ingrid <stephanye.ingrid@gmail.com>
2024-10-04 12:01:56 -0300

    estruturas de repetição

--- respostas/repetição/app.js
+++ respostas/repetição/app.js
@@ -1,15 +1,71 @@
 
 // alert("Hello World");
 
-function receberValor(){
+// function receberValor(){
 
-    let valor;
-    let conteudo = '';
+//     let valor;
+//     let conteudo = '';
 
-    do {
-        valor = prompt("Informe um valor: ");
-        console.log (valor);
+//     do {
+//         valor = prompt("Informe um valor: ");
+//         console.log (valor);
         
-    } while (valor != 0 || valor != null);
-    document.getElementById('resultado').innerHTML += valor;
+//     } while (valor != 0 || valor != null);
+//     document.getElementById('resultado').innerHTML += valor;
+// }
+
+// let menu;
+
+// do{
+//     // Usuário informa uma opção do menu:
+//     menu = Number(prompt(`Informe uma opção: `));
+//     menu = +prompt(`Informe uma opção: `);
+//     menu = parseInt(prompt(`Informe uma opção: `));
+//     menu = parseFloat(prompt(`Informe uma opção: `));
+
+// switch(menu) {
+//     case 0:
+//         console.log("Sair");
+//         break;
+//     case 1:
+//         console.log("Soma");
+//         break;
+//     case 2:
+//         console.log("Subtrai");
+//         break;
+//     case 3:
+//         console.log("Multiplica");
+//         break;
+//     case 4:
+//         console.log("Divide");
+//         break;
+//     default:
+//         console.log("Opção Inválida");
+//         break;
+// }
+
+// } while(menu != 0);
+
+
+
+let menorAltura;
+let maiorAltura;
+
+for (let i = 1; i <= 15; i++) {
+    const altura = Number(prompt(`Informe a altura (em cm): `));
+
+    if(i ==1){
+        menorAltura = altura;
+        maiorAltura = altura
+    }
+
+    if(altura > maiorAltura) {
+        maiorAltura = altura;
+    }
+    if(altura < menorAltura){
+        menorAltura = altura;
+    }
 }
+
+console.log(`A menor altura do grupo : ${menorAltura / 100} m`);
+console.log(`A maior altura do grupo : ${maiorAltura / 100} m`);

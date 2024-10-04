f042c91bfd2ec478be4e3b1eeaad15771ae6abbd
HeyVds <vitorsan64@gmail.com>
2024-10-04 14:07:15 -0300

    exercício 05 rep

--- Atividades VictorM/exercicio-repeticao.js
+++ Atividades VictorM/exercicio-repeticao.js
@@ -33,6 +33,7 @@
 
 
 // exercício 03
+
 // let contadorP = 0;
 // let contadorN = 0;
 // let contador = 0;
@@ -64,36 +65,72 @@
 
 
 // exercício 4
-let numero = 0;
-let contador1 = 0;
-let contador2 = 0;
-let contador3 = 0;
-let contador4 = 0;
 
-while (numero >= 0){
-    numero = Number(prompt("Digite um valor de 0 a 100(digite um valor negativo para parar):"))
-    if (numero < 0 ){
-        break
-    } else if (numero >= 0 && numero <= 25){
-        contador1++;
-    } else if (numero <= 50){
-        contador2++;
-        console.log(contador2);
+// let numero = 0;
+// let contador1 = 0;
+// let contador2 = 0;
+// let contador3 = 0;
+// let contador4 = 0;
+
+// while (numero >= 0){
+//     numero = Number(prompt("Digite um valor de 0 a 100(digite um valor negativo para parar):"))
+//     if (numero < 0 ){
+//         break
+//     } else if (numero >= 0 && numero <= 25){
+//         contador1++;
+//     } else if (numero <= 50){
+//         contador2++;
+//         console.log(contador2);
         
-    } else if(numero <= 75){
-        contador3++;
-    }else if(numero <= 100){
-        contador4++;
-    } else {
+//     } else if(numero <= 75){
+//         contador3++;
+//     }else if(numero <= 100){
+//         contador4++;
+//     } else {
+//         break
+//     }
+// }
+
+// console.log(`Quantidade de números contados foram:
+// entre 0 e 25= ${contador1}
+// entre 26 e 50= ${contador2}
+// entre 51 e 75= ${contador3}
+// entre 76 e 100= ${contador4}`);
+
+
+//exercício 5
+
+let contadorPar = 0;
+let contadorImpar = 0;
+let contador = 0;
+let somaPar = 0;
+let soma = 0;
+let media = 0;
+let mediaPar = 0;
+let numero;
+
+while (numero != 0){
+    numero = Number(prompt("Digite um valor positivo: "))
+    if (numero >0){
+        if (numero % 2 === 0){
+            contadorPar++;
+            somaPar += numero
+        } else{
+            contadorImpar++;
+        }
+    } else{
         break
     }
+    contador++;
+    soma += numero;
 }
 
-console.log(`Quantidade de números contados foram:
-entre 0 e 25= ${contador1}
-entre 26 e 50= ${contador2}
-entre 51 e 75= ${contador3}
-entre 76 e 100= ${contador4}`);
+mediaPar = (somaPar/contadorPar);
+media = (soma/contador);
 
+console.log(`
+A quantidade de número pares: ${contadorPar} e ímpares: ${contadorImpar}; 
+A media dos valor pares enviados foi = ${mediaPar};
+A media geral dos valor enviados foi = ${media};`);
 
-//eercício 5
+// exercício 6
